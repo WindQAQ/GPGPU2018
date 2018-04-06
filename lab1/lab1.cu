cd15198f90860b89b0114d005aeac7620b1afb92
WindQAQ <b03902042@ntu.edu.tw>
2018-04-06 21:39:17 +0800

    refactorize the code

--- lab1/lab1.cu
+++ lab1/lab1.cu
@@ -3,13 +3,14 @@
 #include <algorithm>
 #include "lab1.h"
 
+#define dt 0.5f
+
 static const unsigned W = 512;
 static const unsigned H = 512;
 static const unsigned NFRAME = 240;
 static const int SIZE = W *H;
 static const int THREADS_PER_BLOCK = 256;
 static const int NUMBER_OF_BLOCKS = SIZE / THREADS_PER_BLOCK;
-static const float dt = 1.0f;
 
 enum class Boundary {
 	U, 
@@ -48,12 +49,11 @@ __global__ void add_force(float *d, const float *s)
 	const int x = idx % W, y = idx / W;
 
 	if (not_boundary(x, y)) {
-		// d[idx] += dt * s[idx];
-		d[idx] += 1.0 * s[idx];
+		d[idx] += dt * s[idx];
 	}
 }
 
-__global__ void transport(float *d, float *d0, float *u, float *v, const float dissipation)
+__global__ void transport(float *d, const float *d0, const float *u, const float *v, const float dissipation)
 {
 	const int idx = threadIdx.x + blockIdx.x * blockDim.x;
 	const int x = idx % W, y = idx / W;
@@ -62,8 +62,8 @@ __global__ void transport(float *d, float *d0, float *u, float *v, const float d
 
 	if (not_boundary(x, y)) {
 		// trace practicle
-		nx = x - 1.0 * u[idx];
-		ny = y - 1.0 * v[idx];
+		nx = x - dt * u[idx];
+		ny = y - dt * v[idx];
 
 		// clip value out of boundary
 		nx = clamp(nx, 0.5, W - 1.5);
@@ -82,8 +82,8 @@ __global__ void get_divergence(const float *u, const float *v, float *div)
 
 	if (not_boundary(x, y)) {
 		// calculate gradient from neighbors (difference)
-		div[idx] = -0.5 * ( (u[idx + 1] - u[idx - 1]) / (W - 2) + 
-					(v[x + (y - 1) * W] - v[x + (y + 1) * W]) / (H - 2) );
+		div[idx] = -0.5 * ( (u[idx + 1] - u[idx - 1]) +
+					(v[x + (y + 1) * W] - v[x + (y - 1) * W]) );
 	}
 }
 
@@ -104,8 +104,8 @@ __global__ void update_velocity(float *u, float *v, const float *p)
 	const int x = idx % W, y = idx / W;
 
 	if (not_boundary(x, y)) {
-		u[idx] -= 0.5 * (p[idx + 1] - p[idx - 1]) * (W - 2);
-		v[idx] -= 0.5 * (p[x + (y + 1) * W] - p[x + (y - 1) * W]) * (H - 2);
+		u[idx] -= 0.5 * (p[idx + 1] - p[idx - 1]);
+		v[idx] -= 0.5 * (p[x + (y + 1) * W] - p[x + (y - 1) * W]);
 	}
 }
 
@@ -145,24 +145,48 @@ __global__ void init_source(float *u, float *v, float *d)
 	const int idx = threadIdx.x + blockIdx.x * blockDim.x;
 	const int x = idx % W, y = idx / W;
 
-	int dx = x - W / 2, dy = y - H / 2;
-
+	int dx = x - W / 2, dy = -(y - H / 2);
 	int distance = dx * dx + dy * dy;
+
 	if (distance <= 10000) {
+		u[idx] = 5.0 * -dy;
+		v[idx] = 5.0 * dx;
+	}
+
+	dx = x - W / 2 - 100, dy = -(y - H / 2) - 100;
+	distance = dx * dx + dy * dy;
+	if (distance <= 900) {
 		d[idx] = 10.0;
 	}
 
-	u[idx] = 2 * dy / H;
-	v[idx] = 2 * dx / W;
+	dx = x - W / 2 + 100, dy = -(y - H / 2) - 100;
+	distance = dx * dx + dy * dy;
+	if (distance <= 900) {
+		d[idx] = 10.0;
+	}
+
+	dx = x - W / 2 - 100, dy = -(y - H / 2) + 100;
+	distance = dx * dx + dy * dy;
+	if (distance <= 900) {
+		d[idx] = 10.0;
+	}
+
+	dx = x - W / 2 + 100, dy = -(y - H / 2) + 100;
+	distance = dx * dx + dy * dy;
+	if (distance <= 900) {
+		d[idx] = 10.0;
+	}
 }
 
 __host__ void project(float *u, float *v, float *p, float *p0, float *ptemp, float *div)
 {
 	get_divergence <<<NUMBER_OF_BLOCKS, THREADS_PER_BLOCK>>> (u, v, div);
-
 	cudaMemset(p, 0, SIZE * sizeof(float));
 
-	for (int i = 0; i < 80; i++) {
+	set_boundary <<<NUMBER_OF_BLOCKS, THREADS_PER_BLOCK>>> (div, Boundary::D);
+	set_boundary <<<NUMBER_OF_BLOCKS, THREADS_PER_BLOCK>>> (p, Boundary::D);
+
+	for (int i = 0; i < 50; i++) {
 		linear_solve <<<NUMBER_OF_BLOCKS, THREADS_PER_BLOCK>>> (u, v, div, p0, p);
 
 		ptemp = p0; p0 = p; p = ptemp;
@@ -173,6 +197,12 @@ __host__ void project(float *u, float *v, float *p, float *p0, float *ptemp, flo
 	update_velocity <<<NUMBER_OF_BLOCKS, THREADS_PER_BLOCK>>> (u, v, p);
 	set_boundary <<<NUMBER_OF_BLOCKS, THREADS_PER_BLOCK>>> (u, Boundary::U); 
 	set_boundary <<<NUMBER_OF_BLOCKS, THREADS_PER_BLOCK>>> (v, Boundary::V);
+}
+
+__host__ void advect(float *d, const float *d0, const float *u, const float *v, const float dissipation, Boundary mode)
+{
+	transport <<<NUMBER_OF_BLOCKS, THREADS_PER_BLOCK>>> (d, d0, u, v, dissipation);
+	set_boundary <<<NUMBER_OF_BLOCKS, THREADS_PER_BLOCK>>> (d, mode);
 }
 
 struct Lab1VideoGenerator::Impl {
@@ -212,6 +242,8 @@ Lab1VideoGenerator::Lab1VideoGenerator(): impl(new Impl) {
 	cudaMemset(impl->u_source, 0, SIZE * sizeof(float));
 	cudaMemset(impl->v_source, 0, SIZE * sizeof(float));
 	cudaMemset(impl->d_source, 0, SIZE * sizeof(float));
+
+	init_source <<<NUMBER_OF_BLOCKS, THREADS_PER_BLOCK>>> (impl->u_source, impl->v_source, impl->d_source);
 }
 
 Lab1VideoGenerator::~Lab1VideoGenerator() {
@@ -235,34 +267,38 @@ void Lab1VideoGenerator::get_info(Lab1VideoInfo &info)
 
 void Lab1VideoGenerator::Generate(uint8_t *yuv) 
 {
-	float *m = new float[SIZE];
-	uint8_t *n = new uint8_t[SIZE];
-
-	if (impl->t == 0) {
-		init_source <<<NUMBER_OF_BLOCKS, THREADS_PER_BLOCK>>> (impl->u_source, impl->v_source, impl->d_source);
-	}
-
 	// velocity step
 	add_force <<<NUMBER_OF_BLOCKS, THREADS_PER_BLOCK>>> (impl->u0, impl->u_source);
 	add_force <<<NUMBER_OF_BLOCKS, THREADS_PER_BLOCK>>> (impl->v0, impl->u_source);
 
-	add_force <<<NUMBER_OF_BLOCKS, THREADS_PER_BLOCK>>> (impl->d0, impl->d_source);
+	set_boundary <<<NUMBER_OF_BLOCKS, THREADS_PER_BLOCK>>> (impl->u0, Boundary::U);
+	set_boundary <<<NUMBER_OF_BLOCKS, THREADS_PER_BLOCK>>> (impl->v0, Boundary::V);
 
-	transport <<<NUMBER_OF_BLOCKS, THREADS_PER_BLOCK>>> (impl->u, impl->u0, impl->u0, impl->v0, 1.0);
-	transport <<<NUMBER_OF_BLOCKS, THREADS_PER_BLOCK>>> (impl->v, impl->v0, impl->u0, impl->v0, 1.0);
-
-	transport <<<NUMBER_OF_BLOCKS, THREADS_PER_BLOCK>>> (impl->d, impl->d0, impl->u, impl->v, 0.995);
-
-	set_boundary <<<NUMBER_OF_BLOCKS, THREADS_PER_BLOCK>>> (impl->u, Boundary::U);
-	set_boundary <<<NUMBER_OF_BLOCKS, THREADS_PER_BLOCK>>> (impl->v, Boundary::V);
+	advect(impl->u, impl->u0, impl->u0, impl->v0, 1.0, Boundary::U);
+	advect(impl->v, impl->v0, impl->u0, impl->v0, 1.0, Boundary::V);
 
 	project(impl->u, impl->v, impl->p, impl->p0, impl->ptemp, impl->div);
 
+	// scalar step
+	add_force <<<NUMBER_OF_BLOCKS, THREADS_PER_BLOCK>>> (impl->d0, impl->d_source);
+
+	advect(impl->d, impl->d0, impl->u, impl->v, 0.995, Boundary::D);
+
+	// copy to frame
+	float *m = new float[SIZE];
+	uint8_t *n = new uint8_t[SIZE];
+
 	cudaMemcpy(m, impl->d, SIZE * sizeof(float), cudaMemcpyDeviceToHost);
 
-	std::transform(m, m + SIZE, n, [](uint8_t a) -> float {
-		return (uint8_t) std::max(std::min((float)a, 255.0f), 0.0f);
-	});
+	for (int i = 0; i < H; i++) {
+		for (int j = 0; j < W; j++) {
+			int idx = i * W + j;
+			if (i == 0 || j == 0 || i == H - 1 || j == W - 1) n[idx] = (uint8_t) std::max(std::min((float)m[idx], 255.0f), 0.0f);
+			else {
+				n[idx] = (uint8_t) std::max(std::min((float) (m[idx] + m[idx + 1] + m[idx - 1] + m[j + (i - 1) * W] + m[j + (i + 1) * W]) * 0.20f, 255.0f), 0.0f);
+			}
+		}
+	}
 
 	cudaMemcpy(yuv, n, SIZE * sizeof(uint8_t), cudaMemcpyHostToDevice);
 	cudaMemset(yuv + SIZE, 128, SIZE / 2 * sizeof(uint8_t));
