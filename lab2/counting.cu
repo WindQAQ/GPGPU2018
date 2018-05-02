2f8479d0915730d3cc74680cec1583cc14895f91
WindQAQ <b03902042@ntu.edu.tw>
2018-05-02 18:20:24 +0800

    thrust-based and naive O(nk) method

---
+++ lab2/counting.cu
@@ -0,0 +1,59 @@
+#include "counting.h"
+#include <cstdio>
+#include <cassert>
+#include <thrust/scan.h>
+#include <thrust/transform.h>
+#include <thrust/functional.h>
+#include <thrust/device_ptr.h>
+#include <thrust/execution_policy.h>
+
+const int MAX_SIZE = (40 << 20) + 100;
+const int NUM_THREADS = 128;
+
+__device__ __host__ int CeilDiv(int a, int b) { return (a-1)/b + 1; }
+__device__ __host__ int CeilAlign(int a, int b) { return CeilDiv(a, b) * b; }
+
+struct F: public thrust::unary_function<char, int> {
+    __device__ __host__ int operator() (char a) { return (a == '\n')? 0: 1; }
+};
+
+template <typename T>
+void print(T* a, const int size)
+{
+    thrust::device_ptr<T> p(a);
+    thrust::device_vector<T> v(p, p + size);
+
+    thrust::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+}
+
+void CountPosition1(const char *text, int *pos, int text_size)
+{
+    F f;
+    thrust::transform(thrust::device, text, text + text_size, pos, f);
+    thrust::inclusive_scan_by_key(thrust::device, pos, pos + text_size, pos, pos);
+}
+
+namespace Naive {
+    // O(nk) method
+    __global__ void count(const char *text, int *pos, const int text_size)
+    {
+        int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+        if (idx < text_size) {
+            if (idx == 0 || (text[idx - 1] == '\n' && text[idx] != '\n')) {
+                for (int i = idx, k = 1; i < text_size && text[i] != '\n'; i++, k++) {
+                    pos[i] = k;
+                }
+            }
+            else if (text[idx] == '\n') {
+                pos[idx] = 0;
+            }
+        }
+    }
+}
+
+void CountPosition2(const char *text, int *pos, int text_size)
+{
+    Naive::count <<<MAX_SIZE / NUM_THREADS + 10, NUM_THREADS>>> (text, pos, text_size);
+}
