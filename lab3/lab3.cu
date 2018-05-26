e55c3d9fe37dcf948cd9180ad54c97bafe676c74
WindQAQ <b03902042@ntu.edu.tw>
2018-05-26 20:22:46 +0800

    sor acceleration

--- lab3/lab3.cu
+++ lab3/lab3.cu
@@ -1,5 +1,8 @@
 #include "lab3.h"
 #include <cstdio>
+#include <string>
+
+const std::string mode = "sor";
 
 __constant__ int dir[][2] = {
 	{0, -1},
@@ -118,12 +121,10 @@ namespace baseline {
 			for (int i = 0; i < 4; i++) {
 				xn = xt + dir[i][0], yn = yt + dir[i][1];
 				neighbor_idx = to_1dindex(wt, xn, yn);
-				if (in_boundary(wt, ht, xn, yn)) {
-					if (mask[neighbor_idx] > 127.0f) {
-						r += input[3 * neighbor_idx + 0];
-						g += input[3 * neighbor_idx + 1];
-						b += input[3 * neighbor_idx + 2];
-					}
+				if (in_boundary(wt, ht, xn, yn) && mask[neighbor_idx] > 127.0f) {
+					r += input[3 * neighbor_idx + 0];
+					g += input[3 * neighbor_idx + 1];
+					b += input[3 * neighbor_idx + 2];
 				}
 			}
 
@@ -179,6 +180,89 @@ namespace baseline {
 	}
 }
 
+namespace sor {
+
+	__global__ void PoissonImageCloningIteration(
+		const float *fixed,
+		const float *mask,
+		const float *input,
+		float *output,
+		const int wt, const int ht,
+		const float omega
+	)
+	{
+		const int yt = blockIdx.y * blockDim.y + threadIdx.y;
+		const int xt = blockIdx.x * blockDim.x + threadIdx.x;
+		const int center_idx = to_1dindex(wt, xt, yt);
+
+		if (in_boundary(wt, ht, xt, yt) && mask[center_idx] > 127.0f) {
+			int neighbor_idx;
+			int xn, yn;
+			float r = fixed[3 * center_idx + 0], g = fixed[3 * center_idx + 1], b = fixed[3 * center_idx + 2];
+
+			for (int i = 0; i < 4; i++) {
+				xn = xt + dir[i][0], yn = yt + dir[i][1];
+				neighbor_idx = to_1dindex(wt, xn, yn);
+				if (in_boundary(wt, ht, xn, yn) && mask[neighbor_idx] > 127.0f) {
+					r += input[3 * neighbor_idx + 0];
+					g += input[3 * neighbor_idx + 1];
+					b += input[3 * neighbor_idx + 2];
+				}
+			}
+
+			output[3 * center_idx + 0] = omega * r / 4 + (1 - omega) * output[3 * center_idx + 0];
+			output[3 * center_idx + 1] = omega * g / 4 + (1 - omega) * output[3 * center_idx + 1];
+			output[3 * center_idx + 2] = omega * b / 4 + (1 - omega) * output[3 * center_idx + 2];
+		}
+	}
+
+	void PoissonImageCloning(
+		const float *background,
+		const float *target,
+		const float *mask,
+		float *output,
+		const int wb, const int hb, const int wt, const int ht,
+		const int oy, const int ox
+	)
+	{
+		float *fixed, *buf1, *buf2;
+		cudaMalloc(&fixed, 3 * wt * ht * sizeof(float));
+		cudaMalloc(&buf1, 3 * wt * ht * sizeof(float));
+		cudaMalloc(&buf2, 3 * wt * ht * sizeof(float));
+
+		// initialize the iteration
+		dim3 gdim(CeilDiv(wt, 32), CeilDiv(ht, 16)), bdim(32, 16);
+		baseline::CalculateFixed <<<gdim, bdim>>> (
+			background, target, mask, fixed,
+			wb, hb, wt, ht, oy, ox
+		);
+		cudaMemcpy(buf1, target, 3 * wt * ht * sizeof(float), cudaMemcpyDeviceToDevice);
+		cudaMemcpy(buf2, target, 3 * wt * ht * sizeof(float), cudaMemcpyDeviceToDevice);
+
+		// iterate
+		for (int i = 0; i < 10000; ++i) {
+			PoissonImageCloningIteration <<<gdim, bdim>>> (
+				fixed, mask, buf1, buf2, wt, ht, 1.9
+			);
+			PoissonImageCloningIteration <<<gdim, bdim>>> (
+				fixed, mask, buf2, buf1, wt, ht, 1.9
+			);
+		}
+
+		// copy the image back
+		cudaMemcpy(output, background, 3 * wb * hb * sizeof(float), cudaMemcpyDeviceToDevice);
+		SimpleClone <<<gdim, bdim>>> (
+			background, buf1, mask, output,
+			wb, hb, wt, ht, oy, ox
+		);
+
+		// clean up
+		cudaFree(fixed);
+		cudaFree(buf1);
+		cudaFree(buf2);
+	}
+}
+
 void PoissonImageCloning (
 	const float *background,
 	const float *target,
@@ -188,14 +272,23 @@ void PoissonImageCloning (
 	const int oy, const int ox
 )
 {
-	// cudaMemcpy(output, background, wb*hb*sizeof(float)*3, cudaMemcpyDeviceToDevice);
-	// SimpleClone<<<dim3(CeilDiv(wt,32), CeilDiv(ht,16)), dim3(32,16)>>>(
-	// 	background, target, mask, output,
-	// 	wb, hb, wt, ht, oy, ox
-	// );
-
-	baseline::PoissonImageCloning(
-		background, target, mask, output,
-		wb, hb, wt, ht, oy, ox
-	);
+	if (mode == "simple") {
+		cudaMemcpy(output, background, wb*hb*sizeof(float)*3, cudaMemcpyDeviceToDevice);
+		SimpleClone<<<dim3(CeilDiv(wt,32), CeilDiv(ht,16)), dim3(32,16)>>>(
+			background, target, mask, output,
+			wb, hb, wt, ht, oy, ox
+		);
+	}
+	else if (mode == "baseline") {
+		baseline::PoissonImageCloning(
+			background, target, mask, output,
+			wb, hb, wt, ht, oy, ox
+		);
+	}
+	else if (mode == "sor") {
+		sor::PoissonImageCloning(
+			background, target, mask, output,
+			wb, hb, wt, ht, oy, ox
+		);
+	}
 }
