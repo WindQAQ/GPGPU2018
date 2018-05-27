f534dbf2577cc2221a9e60f8e93c306d94cdf5ff
WindQAQ <b03902042@ntu.edu.tw>
2018-05-27 19:45:21 +0800

    hierarchical acceleration

--- lab3/lab3.cu
+++ lab3/lab3.cu
@@ -2,7 +2,7 @@
 #include <cstdio>
 #include <string>
 
-const std::string mode = "sor";
+const std::string mode = "hierarchical";
 
 __constant__ int dir[][2] = {
 	{0, -1},
@@ -263,6 +263,156 @@ namespace sor {
 	}
 }
 
+namespace hierarchical {
+
+	__global__ void CalculateFixed(
+		const float *background,
+		const float *target,
+		const float *mask,
+		float *fixed,
+		const int wb, const int hb, const int wt, const int ht,
+		const int oy, const int ox, const int scale
+	)
+	{
+		const int yt = blockIdx.y * blockDim.y + threadIdx.y;
+		const int xt = blockIdx.x * blockDim.x + threadIdx.x;
+		const int center_idx = to_1dindex(wt, xt, yt);
+
+		if (in_boundary(wt, ht, xt, yt) && xt % scale == 0 && yt % scale == 0) {
+			int neighbor = 0, neighbor_idx, background_idx;
+			int xb, yb, xn, yn;
+			float r = 0.0f, g = 0.0f, b = 0.0f;
+
+			for (int i = 0; i < 4; i++) {
+				xn = xt + dir[i][0] * scale, yn = yt + dir[i][1] * scale;
+				xb = xn + ox, yb = yn + oy;
+				neighbor_idx = to_1dindex(wt, xn, yn);
+				background_idx = to_1dindex(wb, xb, yb);
+				if (in_boundary(wt, ht, xn, yn)) {
+					r -= target[3 * neighbor_idx + 0];
+					g -= target[3 * neighbor_idx + 1];
+					b -= target[3 * neighbor_idx + 2];
+
+					if (mask[neighbor_idx] <= 127.0f && in_boundary(wb, hb, xb, yb)) {
+						r += background[3 * background_idx + 0],
+						g += background[3 * background_idx + 1],
+						b += background[3 * background_idx + 2];
+					}
+					neighbor++;
+				}
+				else if (in_boundary(wb, hb, xb, yb)) {
+					r += background[3 * background_idx + 0],
+					g += background[3 * background_idx + 1],
+					b += background[3 * background_idx + 2];
+				}
+			}
+
+			r += neighbor * target[3 * center_idx + 0];
+			g += neighbor * target[3 * center_idx + 1];
+			b += neighbor * target[3 * center_idx + 2];
+
+			for (int y = yt; y - yt < scale && y < ht; y++) {
+				for (int x = xt; x - xt < scale && x < wt; x++) {
+					const int idx = to_1dindex(wt, x, y);
+					fixed[3 * idx + 0] = r;
+					fixed[3 * idx + 1] = g;
+					fixed[3 * idx + 2] = b;
+				}
+			}
+		}
+	}
+
+	__global__ void PoissonImageCloningIteration(
+		const float *fixed,
+		const float *mask,
+		const float *input,
+		float *output,
+		const int wt, const int ht,
+		const int scale
+	)
+	{
+		const int yt = blockIdx.y * blockDim.y + threadIdx.y;
+		const int xt = blockIdx.x * blockDim.x + threadIdx.x;
+		const int center_idx = to_1dindex(wt, xt, yt);
+
+		if (in_boundary(wt, ht, xt, yt) && xt % scale == 0 && yt % scale == 0 && mask[center_idx] > 127.0f) {
+			int neighbor_idx;
+			int xn, yn;
+			float r = fixed[3 * center_idx + 0], g = fixed[3 * center_idx + 1], b = fixed[3 * center_idx + 2];
+
+			for (int i = 0; i < 4; i++) {
+				xn = xt + dir[i][0] * scale, yn = yt + dir[i][1] * scale;
+				neighbor_idx = to_1dindex(wt, xn, yn);
+				if (in_boundary(wt, ht, xn, yn) && mask[neighbor_idx] > 127.0f) {
+					r += input[3 * neighbor_idx + 0];
+					g += input[3 * neighbor_idx + 1];
+					b += input[3 * neighbor_idx + 2];
+				}
+			}
+
+			r /= 4;
+			g /= 4;
+			b /= 4;
+
+			for (int y = yt; y - yt < scale && y < ht; y++) {
+				for (int x = xt; x - xt < scale && x < wt; x++) {
+					const int idx = to_1dindex(wt, x, y);
+					output[3 * idx + 0] = r;
+					output[3 * idx + 1] = g;
+					output[3 * idx + 2] = b;
+				}
+			}
+		}
+	}
+
+	void PoissonImageCloning(
+		const float *background,
+		const float *target,
+		const float *mask,
+		float *output,
+		const int wb, const int hb, const int wt, const int ht,
+		const int oy, const int ox
+	)
+	{
+		float *fixed, *buf1, *buf2;
+		cudaMalloc(&fixed, 3 * wt * ht * sizeof(float));
+		cudaMalloc(&buf1, 3 * wt * ht * sizeof(float));
+		cudaMalloc(&buf2, 3 * wt * ht * sizeof(float));
+
+		// initialize the iteration
+		dim3 gdim(CeilDiv(wt, 32), CeilDiv(ht, 16)), bdim(32, 16);
+		cudaMemcpy(buf1, target, 3 * wt * ht * sizeof(float), cudaMemcpyDeviceToDevice);
+
+		// iterate
+		for (int scale = 8; scale >= 1; scale >>= 1) {
+			CalculateFixed <<<gdim, bdim>>> (
+				background, target, mask, fixed,
+				wb, hb, wt, ht, oy, ox, scale
+			);
+			for (int i = 0; i < 100 * scale; ++i) {
+				PoissonImageCloningIteration <<<gdim, bdim>>> (
+					fixed, mask, buf1, buf2, wt, ht, scale
+				);
+				PoissonImageCloningIteration <<<gdim, bdim>>> (
+					fixed, mask, buf2, buf1, wt, ht, scale
+				);
+			}
+		}
+
+		// copy the image back
+		cudaMemcpy(output, background, 3 * wb * hb * sizeof(float), cudaMemcpyDeviceToDevice);
+		SimpleClone <<<gdim, bdim>>> (
+			background, buf1, mask, output,
+			wb, hb, wt, ht, oy, ox
+		);
+
+		// clean up
+		cudaFree(fixed);
+		cudaFree(buf1);
+		cudaFree(buf2);
+	}
+}
+
 void PoissonImageCloning (
 	const float *background,
 	const float *target,
@@ -287,6 +437,12 @@ void PoissonImageCloning (
 	}
 	else if (mode == "sor") {
 		sor::PoissonImageCloning(
+			background, target, mask, output,
+			wb, hb, wt, ht, oy, ox
+		);
+	}
+	else if (mode == "hierarchical") {
+		hierarchical::PoissonImageCloning(
 			background, target, mask, output,
 			wb, hb, wt, ht, oy, ox
 		);
